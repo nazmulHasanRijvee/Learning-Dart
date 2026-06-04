9e9284a0a1897bf0a04290ca1d3379781af7f591
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-06-04 20:38:34 +0600

    Added named parameter methods in function

--- learning22_function_in_dart.dart
+++ learning22_function_in_dart.dart
@@ -16,7 +16,7 @@ String todayOffer(){
 }
 
 
-/// This one returns a doule value
+/// This one returns a double value
 double calculateArea(double length, double width){
   return length * width;
 }
@@ -33,6 +33,7 @@ int calclulateFare(int distance) => distance*10;
 
 
 /// Optional parameter using []
+// name is optional here
 void calculateAreaPrm(double length, double width, [String? name]) {
   double area = length * width;
 
@@ -43,6 +44,46 @@ void calculateAreaPrm(double length, double width, [String? name]) {
   }
 }
 
+
+/// Named Parameters with positioned parameters
+/// using {double width = 0.0} or required keyword
+// The order matters:
+// Positioned parameters first, then named parameter (not at first)
+// We can also provide default values for methods if no parameter is given
+// in named parameters (VERY USEFUL)
+void namedMethod(int age, double height, {String name = 'Unknown'}) {
+
+  print('My name is $name and age is $age, I\'m $height feet tall');
+
+}
+
+
+/// required keyword in named parameters
+// using required keyword we don't have to set a default value for the parameters
+// it is required to assign the value, if not compiler will show error
+void requiredNamedMethod (int age, String? name,  {required double height}) {
+
+  if (name != null){
+    print('I am $age years old, my height is ${height.toStringAsFixed(2)}, Name is $name');
+  } else {
+    print('I am $age years old, my height is ${height.toStringAsFixed(2)}, No name');
+  }
+
+}
+
+
+/// Optional parameters with named parameters
+// In named parameter just make the parameter nullable and it becomes optional
+// no required keyword or value assignment needed
+void optionalNamedMethod ({int age = 0, String? name, required double height}) {
+
+  if (name != null){
+    print('I am $age years old, my height is ${height.toStringAsFixed(2)}, Name is $name');
+  } else {
+    print('I am $age years old, my height is ${height.toStringAsFixed(2)}, No name');
+  }
+
+}
 
 void main () {
 
@@ -73,6 +114,18 @@ void main () {
   /// Optional parameter function
   calculateAreaPrm(10.5, 5.6);
   calculateAreaPrm(11.12, 12.5, "Mirpur");
-  
+
+
+  /// Named Parameter function or method
+  // with positioned parameters
+  namedMethod(23, 6.1, name: 'Nazmul' );
+
+  // required keyword in named parameters
+  requiredNamedMethod(23, null, height: 6.123);
+
+  // Optional parameters with named parameters
+  optionalNamedMethod(age: 24, height: 6.657);
+  // using or passing value to optional parameter
+  optionalNamedMethod(age: 24, name: 'Nazmul', height: 5.121);
 
 }
