4670cdf93e7777be05747de310aa0f19f0f0b56b
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-06-23 23:07:17 +0600

    solved first non-repeating character in the string

---
+++ competitive_programming/cp9_first_non_repeating_character_in_the_string.dart
@@ -0,0 +1,17 @@
+void main () {
+
+  /// Find the first non-repeating character in a string
+  final String text = "loveleetcode";
+  final Map<String, int> freq = {};
+  for(int i = 0; i < text.length; i++){
+    if(freq[text[i]] == null) freq[text[i]] = 0;
+    freq[text[i]] = freq[text[i]]! + 1;
+  }
+  for(int i = 0; i < text.length; i++) {
+    if(freq[text[i]] == 1) {
+      print(text[i]); // answer is v
+      break;
+    }
+  }
+
+}
