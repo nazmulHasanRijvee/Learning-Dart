cbb90032a503bedfffda2ed9d8d3c74058d39701
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-06-30 23:28:25 +0600

    Solved read and write two pointer pattern

---
+++ competitive_programming/cp13_two_pointer_read_and_write_pattern_moving_zeroes.dart
@@ -0,0 +1,24 @@
+/// Moving all the zeroes in an array to the last using two pointer
+// Constraints:
+// Don't create another list or use.pop() method
+// Think in terms of two pointer
+// Read and write two pointer pattern
+void main() {
+
+  final numbers = [0, 1, 0, 3, 12];
+  int write = 0;
+
+  for(int i = 0; i <numbers.length; i++) {
+    if(numbers[i] != 0) {
+      numbers[write] = numbers[i];
+      write++;
+    }
+  }
+
+  for(int i = write; i < numbers.length; i++) {
+    numbers[i] = 0;
+  }
+
+  print(numbers);
+
+}
---
+++ testing.dart
@@ -0,0 +1,6 @@
+void main () {
+
+  final value = String.fromCharCodes([65,66, 67]);
+  print(value);
+
+}
