d47f862074c3d6977cde708e0345479008aabef6
Nazmul Hasan <141444914+goingdark007@users.noreply.github.com>
2026-08-01 12:48:01 +0600

    Added finally{} block with try-catch

--- learning27_exception_handling_try_catch.dart
+++ learning27_exception_handling_try_catch.dart
@@ -49,7 +49,7 @@ void main () async {
   }
 
 
-  /// We can also throw Exception in catch(e) block and and in another try-catch
+  /// We can also throw Exception in catch(e) block and and  catch in another try-catch
   // block we can catch the exception (called outer exception), which is
   // wrapping the previous one
   try {
@@ -66,6 +66,56 @@ void main () async {
     // We can inspect outerException to see the wrapped original exception
     print('Caught an outer exception: $outerException');
   }
+
+
+  /// finally{} block with try-catch block
+  // finally{} block will run no matter what. It says after running the risky code
+  // run the code in finally. Even if there is error (catch(e) {} runs) or no-error,
+  // finally block runs regardless
+  try {
+
+    final int value = int.parse('a');
+    print(value);
+
+  } catch (e) {
+
+    print('Error: $e');
+
+  } finally {
+
+    print('Code has been run');
+
+  }
+
+  // We can also use finally{} directly with try{} block
+  // Also even if code return from try{} block, finally{} block will run
+  // regardless this is especially useful to ensure reusability.
+  // Like multiple if conditions returns and stops the function but if there
+  // is common code that need to run when function is returned or stooped
+  void doSomething() {
+    final uri = {
+      "approved": true,
+      "request_token" : 'abc1234'
+    };
+
+    try{
+
+      // Instead of writing here print twice, write once in finally block
+      if(uri["approved"] != true) return;
+
+      if(uri["request_token"] == null) return;
+
+      print('Authenticated');
+
+      // Runs regardless, even if returns from try{} block
+    } finally {
+
+      print('Function Stopped, dispose controllers or cancel Streams');
+
+    }
+  }
+
+  doSomething();
 
 
   /// We can use .then() and .catchError as alternatives
